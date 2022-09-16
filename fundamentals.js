a303d3b9f2caa4d9acff3032d83d6d4d9817e5a4
Ethan Roche Woodward <ethanroche17@gmail.com>
2022-09-16 14:17:34 +0100

    Loop constructs.

--- fundamentals.js
+++ fundamentals.js
@@ -9,6 +9,7 @@ const aCar = {
       model: 'Corolla',
       cc: 1.8
     },
+    features : ['Parking assist', 'Alarm', 'Tow-bar'],
     registration : {
       year: "201",
       countyCode: "WD",
@@ -24,9 +25,23 @@ const aCar = {
       shade: "cream",
   },
 }
+
+
   
-  console.log(aCar.owner);
-  console.log(aCar.owner + ' drives a ' + aCar.type.make);
-  console.log("Reg. = " + aCar.registration.year + "-" + aCar.registration.countyCode + "-" + aCar.registration.number);
-  console.log("It is a " + aCar.colour.exterior + " car," + aCar.mileage + " mileage, with " + aCar.colour.interior.texture + " interior");
-  console.log('First owner : ' + aCar.previous_owners[0].name + " living at " + aCar.previous_owners[0].address )
+  // console.log(aCar.owner);
+  // console.log(aCar.owner + ' drives a ' + aCar.type.make);
+  // console.log("Reg. = " + aCar.registration.year + "-" + aCar.registration.countyCode + "-" + aCar.registration.number);
+  // console.log("It is a " + aCar.colour.exterior + " car," + aCar.mileage + " mileage, with " + aCar.colour.interior.texture + " interior");
+  // console.log('First owner : ' + aCar.previous_owners[0].name + " living at " + aCar.previous_owners[0].address )
+
+  for (let i = 0 ; i < aCar.features.length ; i += 1) {
+    console.log(aCar.features[i]) ;
+  } 
+
+  for (let i = 0; i < aCar.previous_owners.length; i ++) {
+    console.log(aCar.previous_owners[i].name + " living at " + aCar.previous_owners[i].address)
+  }
+
+  for (let p in aCar.type)  {
+    console.log(p.toUpperCase() + ' = ' + aCar.type[p] ) ;
+}
